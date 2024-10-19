fbf6666913e31ccbbb618b4a6bf126cd0f087a15
standjeong <andame9999@gmail.com>
2024-10-19 17:33:03 +0900

    style : update header effects

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ const header = document.querySelector('.header');
 const headerHeight = header.getBoundingClientRect().height;
 
 document.addEventListener('scroll', () => {
-  if (window.scrollY > headerHeight && window.innerWidth > 768) {
+  if (window.scrollY > headerHeight) {
     header.classList.add('dark');
   } else {
     header.classList.remove('dark');
